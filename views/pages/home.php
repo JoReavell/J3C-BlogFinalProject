73ad3c602ca1fa2a8e35319bdfd827292d914af9
ClaudiaDanciu <contact@claudiadanciu.com>
2018-04-10 22:35:31 +0100

    login/signup/home
    I added the logic for the login / register / home page

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,4 +1,15 @@
-<p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
+<?php
+// Initialize the session (don't forget to close it on logout)
+session_start();
+ 
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: ../blogUser/login.php");
+  exit;
+}
+?>
+<p>Hello there  <?php if (!empty($_SESSION)) {echo "<h1>Hello " . $_SESSION['username'] . "," . "</h1>";}?>
+<!--<p>Hello there <php echo $first_name . ' ' . $last_name; ?>!<p>-->
 <p>The above data is present to demonstrate the utilisation of variables 
 populated earlier within the page processing</p>
 <p>This is the home page of the blog application</p>
