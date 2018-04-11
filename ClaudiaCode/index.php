6fc755c1f1dde011536195b3d869af6d8f85eb87
ClaudiaDanciu <contact@claudiadanciu.com>
2018-04-11 21:26:32 +0100

    nothing special
    nothing special for master (some pictures and nothing) not to push inside master

--- ClaudiaCode/index.php
+++ ClaudiaCode/index.php
@@ -167,7 +167,7 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 							</div>
                                                    
 						</header>
-                                            <a href="single-post.html" class="image featured"><img src="images/skyfood.jpg" alt="" /></a>
+                                            <a href="single-post.html" class="image featured"><img class="home-img-blog" src="images/skyfood.jpg" alt="" /></a>
 						<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
 						<footer>
 							<ul class="actions">
