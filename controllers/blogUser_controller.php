f99969574249ce3fb7e4f0ed562378a7dd49e4a5
ClaudiaDanciu <contact@claudiadanciu.com>
2018-04-12 12:29:02 +0100

    login
    login

--- controllers/blogUser_controller.php
+++ controllers/blogUser_controller.php
@@ -4,22 +4,10 @@
 
 class BlogUserController {
     
-    
-     
-        //User login functionality
-        //have code to start login here then go to model to do actual login
-        //set session variable with userID
-//        $blogUser = BlogUser::login();
-        //set session variables here too!!
-        
-//        require_once 'views/pages/home.php';
- 
-//Define variables and initialized with empty values
-
-    
+   
     public function login() {
         
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
         require_once('views/blogUser/login.php');
     }else{
         $blogUser = BlogUser::login();
