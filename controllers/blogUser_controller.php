4379d6becba8467410ed87d841fe99251e7f5ed6
ClaudiaDanciu <contact@claudiadanciu.com>
2018-04-12 18:09:37 +0100

    initialize session for login
    it is in header after login.

--- controllers/blogUser_controller.php
+++ controllers/blogUser_controller.php
@@ -9,6 +9,7 @@ class BlogUserController {
         
     if($_SERVER["REQUEST_METHOD"] == "GET"){
         require_once('views/blogUser/login.php');
+        echo "GET";
     }else{
         BlogUser::login();
         $blogUser = BlogUser::login();
