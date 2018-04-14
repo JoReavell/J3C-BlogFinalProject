8f20fc896ad181511e20f407394f72ff2e326889
Jen Patchett <hello@jenniferpatchett.com>
2018-04-14 14:22:46 +0100

    Added link to index page in Developer@Sky logo in navbar in HeaderAfterLogin

--- views/HeaderAfterLogin.php
+++ views/HeaderAfterLogin.php
@@ -29,7 +29,7 @@
 <!--Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand " href="#"><img class="logo" src="views/images/logo.png" alt="logo"></a>
+        <a class="navbar-brand " href="index.php"><img class="logo" src="views/images/logo.png" alt="logo"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
