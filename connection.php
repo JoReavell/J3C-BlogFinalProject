6a53353934de6c57234adc6427c6fd53f421821d
ClaudiaDanciu <contact@claudiadanciu.com>
2018-04-26 01:28:27 +0100

    checking the online database

--- connection.php
+++ connection.php
@@ -8,8 +8,10 @@ class DB {
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=blogDatabase', 'blogUser', 'password1', $pdo_options);
-   
+//        self::$instance = new PDO('mysql:host=localhost;dbname=blogDatabase', 'blogUser', 'password1', $pdo_options);
+        self::$instance = new PDO('arn:aws:rds:eu-west-1:446491677593:db:skydev, mysql:host=skydev.clhot6jhrn6b.eu-west-1.rds.amazonaws.com
+; dbname=SkyDev', 'blogUser', '$2018SkyDev', $pdo_options);
+
       }
       return self::$instance;
     }
